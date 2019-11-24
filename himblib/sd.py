1d495ac11638ef3b66d8977e66792260bc9be178
Enrico Zini <enrico@enricozini.org>
2019-11-24 16:48:03 +0100

    Try to work around race conditions on all the automated things that happen after a partition table has been changed

--- himblib/sd.py
+++ himblib/sd.py
@@ -173,6 +173,9 @@ class SD(Command):
             constraint.maxSize = target_root_size
             disk.maximizePartition(part_root, constraint)
             disk.commit()
+            time.sleep(0.3)
+            self.umount(self.locate())
+            time.sleep(0.3)
 
             subprocess.run(["e2fsck", "-fy", part_root.path], check=True)
             subprocess.run(["resize2fs", part_root.path], check=True)
@@ -188,6 +191,9 @@ class SD(Command):
                     geometry=free_space)
             disk.addPartition(partition=partition, constraint=device.optimalAlignedConstraint)
             disk.commit()
+            time.sleep(0.3)
+            self.umount(self.locate())
+            time.sleep(0.3)
             log.info("%s media partition created", format_gb(free_space.length * device.sectorSize))
 
             # Create exFAT file system
