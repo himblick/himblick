3f9b8889b8bd82a6c777d13c5759291822a34276
Enrico Zini <enrico@enricozini.org>
2020-01-13 10:38:32 +0100

    Added stub for a sync task

--- himblib/player/syncer.py
+++ himblib/player/syncer.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 import os
+import asyncio
 import logging
 
 if TYPE_CHECKING:
@@ -14,6 +15,7 @@ class Syncer:
         self.hostname = hostname
         self.media_dir = media_dir
         self.media_key = os.path.expanduser("~/.ssh/id_media")
+        self.sync_task: Optional[asyncio.Task] = None
 
     def rescan(self):
         log.info("syncer:%s: rescanning %s", self.hostname, self.media_dir.path)
@@ -28,3 +30,17 @@ class Syncer:
             else:
                 log.info("syncer:%s: %s to be synced", self.hostname, fn)
                 to_sync.append(fn)
+
+        if to_sync:
+            if self.sync_task is not None:
+                self.sync_task.cancel()
+                self.sync_task = None
+
+            self.sync_task = asyncio.create_task(self.sync(to_sync))
+
+    async def sync(self, fnames):
+        print("SYNCING", fnames)
+        # Create the .synced file
+        with open(os.path.join(self.media_dir.path, f"{self.hostname}.synced"), "wt"):
+            pass
+        self.sync_task = None
